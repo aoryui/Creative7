ade1b88bb7aaffc2d3b72c023fbf96d17e1f5d93
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-06-13 13:44:15 +0900

    honbannnnn

--- header.php
+++ header.php
@@ -7,5 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>就活タイサくんのページ</title>
     <link rel="stylesheet" href="main.css">
+    <header>
+        <h1>就活タイサくん</h1>
+    </header>
 </head>
 
--- honban.php
+++ honban.php
@@ -5,11 +5,8 @@
 </head>
 <body>
     <?php require_once __DIR__ . '/header.php'; ?>
-
-    <header>
-        <h1>就活タイサくん</h1>
-    </header>
     <main>
+        <div class="honbanmain">
         <div class="container">
             <div class="button-container">
                 <button>模擬試験を開始する</button>
@@ -17,6 +14,7 @@
             <p>ランダムで想定された問題が〇〇個出題されます。<br>
                 問題は1問5分です。時間までに解いてください。<br>
                 時間が過ぎると次の問題に自動的に飛ばされます。</p>
+        </div>
         </div>
     </main>
 </body>
