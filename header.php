e4e6f9029d236a38fa469f61796907d4b568b98c
hisamatunao <kd1329331@st.kobedenshi.ac.jp>
2024-06-14 14:31:08 +0900

    a

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ $username = $_SESSION['userName'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>就活タイサくんのページ</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
     <header>
         <h1>就活タイサくん</h1>
     </header>
