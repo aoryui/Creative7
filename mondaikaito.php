a3c2cd2aa337865c7b849277b7ee15eedc86322b
Hayata Nagano <kd1304087X@st.kobedenshi.ac.jp>
2024-06-14 13:49:44 +0900

    ファビコン変更長野

--- mondaikaito.php
+++ mondaikaito.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="image/icon.png" />
     <title>SPIタイサくん</title>
 </head>
 <body>
