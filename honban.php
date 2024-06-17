472064bad099fe89f1503a71742e0ddfee447e6d
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-06-17 10:58:59 +0900

    mojikaeta

--- honban.php
+++ honban.php
@@ -14,8 +14,8 @@ require_once __DIR__ . '/header.php';
         </div>
         <div class="button-container">
         <input type="button" onclick="location.href='./test.php'" value="模擬試験を開始する"></button>
-        <p>ランダムで想定された問題が〇〇個出題されます。<br>
-           問題は1問5分です。時間までに解いてください。<br>
+        <p>ランダムで想定された問題が20個出題されます。<br>
+           問題は1問30秒です。時間までに解いてください。<br>
            時間が過ぎると次の問題に自動的に飛ばされます。</p>
         </div>
         </div>
