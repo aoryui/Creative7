b5e70087d16f7632264d0c20e0b506035990c554
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-06-17 14:11:18 +0900

    practice.php変更2

--- practice.php
+++ practice.php
@@ -7,10 +7,9 @@ require_once __DIR__ . '/header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="image/icon.png" />
-    <link rel="stylesheet" href="./css/practice.css">
     <title>SPIタイサくん</title>
 </head>
-
+    <link rel="stylesheet" href="./css/practice.css">
 <body>
     <div class="content">
         <div class="question">
