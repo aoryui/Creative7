1a2c1cb14c8d992f402c4d420630ee38ac3f06ff
Haruki1203 <kd1309743@st.kobedenshi.ac.jp>
2024-06-17 10:57:43 +0900

    ボタンを押したら画面遷移

--- honban.php
+++ honban.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/header.php';
         <div class="content">
         </div>
         <div class="button-container">
-        <button>模擬試験を開始する</button>
+        <input type="button" onclick="location.href='./test.php'" value="模擬試験を開始する"></button>
         <p>ランダムで想定された問題が〇〇個出題されます。<br>
            問題は1問5分です。時間までに解いてください。<br>
            時間が過ぎると次の問題に自動的に飛ばされます。</p>
