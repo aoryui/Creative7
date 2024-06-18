967979d8d10c22d5852cef266786a4243c874d16
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-06-18 10:47:48 +0900

    honban.phpのUI変更

--- honban.php
+++ honban.php
@@ -2,28 +2,34 @@
 require_once __DIR__ . '/header.php';
 ?>
 <!DOCTYPE html>
-<link rel="stylesheet" href="./css/honban.css">
 <html lang="ja">
 <head>
+    <link rel="stylesheet" href="./css/honban.css">
 </head>
 <body>
     <main>
         <div class="honbanmain">
-        <div class="container">
-        <div class="content">
-        </div>
-        <div class="button-container">
-        <input type="button" onclick="location.href='./test.php'" value="模擬試験を開始する"></button>
-        <p>ランダムで想定された問題が20個出題されます。<br>
-           問題は1問30秒です。時間までに解いてください。<br>
-           時間が過ぎると次の問題に自動的に飛ばされます。</p>
-        </div>
-        </div>
+            <div class="container">
+                <div class="content">
+                    <p>ランダムで想定された問題が20個出題されます。<br>
+                    <p></p>
+                       問題は1問30秒です。
+                       <p></p>
+                       時間までに解いてください。<br>
+                    <p></p>
+                       時間が過ぎると次の問題に自動的に飛ばされます。<br>
+                       <p></p>
+                        メモ用紙と筆記用具を用意してください<br></p>
+                </div>
+                <div class="button-container">
+                    <input type="button" onclick="location.href='./test.php'" value="模擬試験を開始する">
+                </div>
+            </div>
         </div>
     </main>
-    ?>
 </body>
 </html>
+
 
 
 
