81c8ce5708338376b95272281c65683776ba2286
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-06-18 11:11:47 +0900

    honban.UI

--- honban.php
+++ honban.php
@@ -11,15 +11,15 @@ require_once __DIR__ . '/header.php';
         <div class="honbanmain">
             <div class="container">
                 <div class="content">
-                    <p>ランダムで想定された問題が20個出題されます。<br>
+                    <p>ランダムで想定された問題が20個出題されます。
                     <p></p>
                        問題は1問30秒です。
-                       <p></p>
-                       時間までに解いてください。<br>
                     <p></p>
-                       時間が過ぎると次の問題に自動的に飛ばされます。<br>
-                       <p></p>
-                        メモ用紙と筆記用具を用意してください<br></p>
+                       時間までに解いてください。
+                    <p></p>
+                       時間が過ぎると次の問題に自動的に飛ばされます。</p>
+                    <p></p>
+                    メモ用紙と筆記用具を用意してください。
                 </div>
                 <div class="button-container">
                     <input type="button" onclick="location.href='./test.php'" value="模擬試験を開始する">
@@ -29,6 +29,7 @@ require_once __DIR__ . '/header.php';
     </main>
 </body>
 </html>
+
 
 
 
