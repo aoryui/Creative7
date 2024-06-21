b0e8a75aec0448c18ea2c713e3a2fb04ff221076
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2024-06-21 11:05:14 +0900

    result型作成

---
+++ frontend/result.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/header.php';
+?>
+
+<!DOCTYPE html>
+<link rel="stylesheet" href="../css/result.css">
+<div class="bar-graph-wrap">
+  <div class="graph">
+  <span class="name">Graph 01</span>
+  <span class="number">88%</span>
+  </div>
+</div>
