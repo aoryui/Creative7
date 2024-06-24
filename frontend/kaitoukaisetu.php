69d5da5139bca608f01f9929295ab26ccabed1c3
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-06-24 13:25:26 +0900

    kaitoukaisetu.php修正

--- frontend/kaitoukaisetu.php
+++ frontend/kaitoukaisetu.php
@@ -15,17 +15,18 @@ require_once __DIR__ . '/header.php';
             <h2>回答</h2>
             <section class="kaitouran">
                 <div class="kaitou">
+                    <p>不正解</p>
                     <strong>貴方の回答:</strong>
-                    <span id="user-kaitou">うんちな未来</span>
+                    <span id="user-kaitou">回答欄</span>
                 </div>
                 <div class="kaitou">
                     <strong>正解:</strong>
-                    <span id="correct-kaitou">うんこな未来</span>
+                    <span id="correct-kaitou">正解欄</span>
                 </div>
             </section>
             <h2>解説</h2>
             <section class="kaiseturan">
-                <p id="kaisetu">以下解説</p>
+                <p id="kaisetu">解説欄</p>
             </section>
         </main> 
         </div>
