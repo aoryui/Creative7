895e05e3de215deae9afee1ddf2381bf50a19457
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2024-09-03 09:31:23 +0900

    デザイン

--- frontend/signup.php
+++ frontend/signup.php
@@ -16,19 +16,19 @@ if (isset($_SESSION['signup_error'])) {
 ?>
 <form action="../backend/touroku.php" method="post" class="form-group">
     <div class="form-group">
-        <label for="username">ユーザー名:</label>
+        <label for="username" id="usernames">ユーザー名:</label>
         <input type="text" id="username" name="username" required>
     </div>
     <div class="form-group">
-        <label for="school">所属学科:</label>
+        <label for="school" id="schools">所属学科:</label>
         <input type="school" id="school" name="school" required>
     </div>
     <div class="form-group">
-        <label for="address">メールアドレス:</label>
+        <label for="address" id="addresses">メールアドレス:</label>
         <input type="address" id="address" name="address" required>
     </div>
     <div class="form-group">
-        <label for="password">パスワード:</label>
+        <label for="password" id="passwords">パスワード:</label>
         <input type="password" id="password" name="password" required>
     </div>
     <button type="submit" id="submit">登録</button>
