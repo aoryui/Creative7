1a471314ab71c51e2a9373fa9d7362c9267ff87d
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2024-09-04 14:50:12 +0900

    ジャンル選択画面デザイン

--- frontend/genre_selection.php
+++ frontend/genre_selection.php
@@ -21,6 +21,7 @@ require_once __DIR__ . '/header.php'; //ヘッダー指定
         <label class="checksAll">
             <input type="checkbox" id="ChecksAllLanguage">言語系
         </label>
+        <div class="lan">
         <label class="language">
             <input type="checkbox"  class="language-checkbox" id="genre1" name="language[]" value="1">二語の関係
         </label>
@@ -34,50 +35,49 @@ require_once __DIR__ . '/header.php'; //ヘッダー指定
             <input type="checkbox"  class="language-checkbox" id="genre4" name="language[]" value="4">文章の整序
         </label>
         <label class="language">
-            <input type="checkbox"  class="language-checkbox" id="genre5" name="language[]" value="5">空欄補充
+            <input type="checkbox"  class="language-checkbox" id="genre5" name="language[]" value="5">空欄の補充
         </label>
+        </div>
 </div>
     <!-- 非言語系 -->
     <div class="contains">
-    <label class="checksAll">
+    <label class="checksAlls">
             <input type="checkbox" id="ChecksAllNonLanguage">非言語系
         </label>
-    <div class="one_line">
-        <label class="non_language">
+        <label class="non_language1">
             <input type="checkbox" class="non-language-checkbox" id="genre6" name="non_language[]" value="1">場合の数
         </label>
-        <label class="non_language">
+        <label class="non_language1">
             <input type="checkbox" class="non-language-checkbox" id="genre7" name="non_language[]" value="2">推論
         </label>
-        <label class="non_language">
+        <label class="non_language1">
             <input type="checkbox" class="non-language-checkbox" id="genre8" name="non_language[]" value="3">割合
         </label>
-        <label class="non_language">
+        <label class="non_language1">
             <input type="checkbox" class="non-language-checkbox" id="genre9" name="non_language[]" value="4">確率
         </label>
-    </div>    
-    <div class="two_line">
-        <label class="non_language">
+        <br>
+    
+        <label class="non_language2">
             <input type="checkbox" class="non-language-checkbox" id="genre10" name="non_language[]" value="5">金額計算
         </label>
-        <label class="non_language">
+        <label class="non_language2">
             <input type="checkbox" class="non-language-checkbox" id="genre11" name="non_language[]" value="6">分担計算
         </label>
-        <label class="non_language">
+        <label class="non_language2">
             <input type="checkbox" class="non-language-checkbox" id="genre12" name="non_language[]" value="7">速度算
         </label>
-        <label class="non_language">
+        <label class="non_language2">
             <input type="checkbox" class="non-language-checkbox" id="genre12" name="non_language[]" value="8">集合
         </label>
-    </div>
-    <div class="three_line">
-        <label class="non_language">
+
+    
+        <label class="non_language3">
             <input type="checkbox" class="non-language-checkbox" id="genre12" name="non_language[]" value="9">表の読み取り
         </label>
-        <label class="non_language">
+        <label class="non_language3">
             <input type="checkbox" class="non-language-checkbox" id="genre12" name="non_language[]" value="10">特殊計算
         </label>
-    </div>
     </div>
     <button class="button" type="submit">選択</button>
     </form>
