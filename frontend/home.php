9601e7bd5a8d6ef56776b86c3da70aa2314be831
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-10-18 14:39:38 +0900

    homepage作成

---
+++ frontend/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ホームページ</title>
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+    <h1>SPIタイサくん</h1>
+    <p>どちらのボタンをクリックしますか？</p>
+    
+    <form action="page1.php" method="get">
+        <button type="submit">管理者画面へ</button>
+    </form>
+    
+    <form action="page2.php" method="get">
+        <button type="submit">ユーザー画面へ</button>
+    </form>
+</body>
+</html>
