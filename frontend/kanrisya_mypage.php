5d7d3aa68d96e948ad5a439ab8ad31930b46fbb9
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2024-10-18 14:58:44 +0900

    管理者画面

--- frontend/kanrisya_mypage.php
+++ frontend/kanrisya_mypage.php
@@ -12,7 +12,31 @@ require_once __DIR__ . '/header_kanrisya.php'; //ヘッダー指定
     <link rel="stylesheet" href="../css/kanrisya_mypage.css">
 </head>
 <body>
+<div class="login-container">
 
+    <div class="introduce-container1">        
+    <img src="../image/icon/kanrisya.png" class="kanrisya1">       
+    <div class="kanrisyakanri">ユーザー情報一覧</div>
+
+    <div class="introduce-container2">
+    <img src="../image/icon/test-icon.png" class="test">
+    <div class="problem">問題作成</div>
+
+
+    </div>
+
+
+
+
+
+</div>
+
+    
+
+
+
+
+</div>
 </body>
 
 </html>
