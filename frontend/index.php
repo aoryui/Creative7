eceb217e6bbe97d4c75f5f922529d4edde9e95f4
SHIMA Tomoya <kd1279692@st.kobedenshi.ac.jp>
2024-10-21 15:31:36 +0900

    文字修正

--- frontend/index.php
+++ frontend/index.php
@@ -26,7 +26,7 @@
     <section class="hero">
         <div class="container">
             <h1>SPI対策して試験合格を確実に</h1>
-            <p>シンプルでゲーム性溢れるSPIサイトです</p>
+            <p>シンプルでゲーム性溢れるSPIサイト</p>
             <a href="kanrisya_login.php" class="kan">管理者ログイン</a>
             <a href="login.php" class="btn">ユーザーログイン</a>
         </div>
