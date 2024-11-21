0eadc078dfc23bb766dc51bfe782461e957c3815
Hayata Nagano <kd1304087X@st.kobedenshi.ac.jp>
2024-11-21 11:18:02 +0900

    ファビコンをホームに反映

--- frontend/home.php
+++ frontend/home.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SPI サイト</title>
     <link rel="stylesheet" href="../css/home.css">
+    <link rel="icon" type="../image/x-icon" href="../image/icon.png" />
 </head>
 <body>
-
     <!-- ヘッダー -->
     <header>
         <div class="container">
