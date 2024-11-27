f66e34a3481ad2b9544d81efaa78be8b374eeba6
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2024-11-27 11:07:16 +0900

    ジャンル選択レスポンシブ

--- frontend/genre_selection.php
+++ frontend/genre_selection.php
@@ -10,6 +10,7 @@ require_once __DIR__ . '/header.php'; //ヘッダー指定
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SPIタイサくん</title>
     <link rel="stylesheet" href="../css/genre_selection.css">
+    <link rel="stylesheet" href="../responsive/genre_selection.css">
 </head>
 
 <body>
