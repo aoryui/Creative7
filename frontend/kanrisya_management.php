8d87df8fa6b45b097001e10ca56b835dec232967
jun12012001 <kd1300563@st.kobedenshi.ac.jp>
2025-01-09 10:11:26 +0900

    宛先完成

--- frontend/kanrisya_management.php
+++ frontend/kanrisya_management.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['userid'])) {
     <a href="generator_test.php"><img src="../image/icon/test-icon.png" id="png" alt="画像作成"></a>
     <a href="question_insert.php"><img src="../image/icon/sinki.png" id="png" alt="問題作成"></a>
     <a href="user_opinion.php"><img src="../image/icon/users_6.png" id="png" alt="ユーザー意見欄"></a>
-    <a href="ここにリンク先.php"><img src="../image/icon/access-lock.png" id="png" alt="アクセス制限"></a>
+    <a href="restrictions.php"><img src="../image/icon/access-lock.png" id="png" alt="アクセス制限"></a>
     <div>
     <p class="left1">ユーザー情報一覧</p>
     <p class="left2">問題一覧</p>
