f7169ff49e298e40b533d55de8b9b368ed99f930
Hayata Nagano <kd1304087X@st.kobedenshi.ac.jp>
2025-01-09 10:08:30 +0900

    アクセス制限追加

--- frontend/kanrisya_management.php
+++ frontend/kanrisya_management.php
@@ -27,12 +27,14 @@ if (isset($_SESSION['userid'])) {
     <a href="generator_test.php"><img src="../image/icon/test-icon.png" id="png" alt="画像作成"></a>
     <a href="question_insert.php"><img src="../image/icon/sinki.png" id="png" alt="問題作成"></a>
     <a href="user_opinion.php"><img src="../image/icon/users_6.png" id="png" alt="ユーザー意見欄"></a>
+    <a href="ここにリンク先.php"><img src="../image/icon/access-lock.png" id="png" alt="アクセス制限"></a>
     <div>
     <p class="left1">ユーザー情報一覧</p>
     <p class="left2">問題一覧</p>
     <p class="left3">画像作成</p>
     <p class="left4">問題作成</p>
     <p class="left5">ユーザー意見欄</p>
+    <p class="left6">アクセス制限</p>
     </div>
     </div>
 </body>
