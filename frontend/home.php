f6131e58695248cbcf392e3d78cbfa7cb148e914
HIGASINE Ryusei <kd1336693@st.kobedenshi.ac.jp>
2025-01-15 09:27:40 +0900

    レスポンシブ

--- frontend/home.php
+++ frontend/home.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, initial-scale=1">
     <title>SPI サイト</title>
     <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="../responsive/home.css">
